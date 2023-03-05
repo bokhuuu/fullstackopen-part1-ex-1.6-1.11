de1997dfdbf85445203f386cea2dc5f0e4c43663
bokhuuu <zurabokhua@gmail.com>
2023-03-05 15:38:08 +0400

    exercises 1.6-1.11

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
 import { useState } from 'react'
 
-const StatisticsOfOptionGood = (props) => {
-  return (
-    <p>good {props.optionGood}</p>
-  )
-}
-
 const Button = (props) => {
   return (
-    <button onClick={props.handleClickGood} />
+    <button onClick={props.handleClick}>{props.text}</button>
   )
 }
 
-const StatisticsOfOptionNeutral = (props) => {
+const StisticLine = (props) => {
   return (
-    <p>neutral {props.optionNeutral}</p>
+    <p>{props.text} {props.statistic}</p>
   )
 }
-
-const StatisticsOfOptionBad = (props) => {
-  return (
-    <p>bad {props.optionBad}</p>
-  )
-}
-
-const StatisticsAll = (props) => {
-  return (
-    <p>all {props.all}</p>
-  )
-}
-
-const StatisticsAverage = (props) => {
-  return (
-    <p>average {props.average}</p>
-  )
-}
-
-const StatisticsPositive = (props) => {
-  return (
-    <p>positive {props.positive}</p>
-  )
-}
-
-const NoFeedback = (props) => {
-  return (
-    <div>
-      {props.all == 0 ? <p>no feedack given</p> : null}
-    </div>
-  )
-}
-
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -60,31 +21,24 @@ const App = () => {
   const average = (good + neutral + bad) / 3
   const positive = good / (good + neutral + bad) * 100
 
-  const handleClickGood = () => {
-    return setGood(good + 1)
-  }
+  const handleClickGood = () => setGood(good + 1)
+  const handleClickNeutral = () => setNeutral(neutral + 1)
+  const handleClickBad = () => setBad(bad + 1)
 
-  const handleClickNeutral = () => {
-    return setNeutral(neutral + 1)
-  }
-
-  const handleClickBad = () => {
-    return setBad(bad + 1)
-  }
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleClickGood}>good</button>
-      <button onClick={handleClickNeutral}>neutral</button>
-      <button onClick={handleClickBad}>bad</button>
-      <h1>statistics</h1>
-      <StatisticsOfOptionGood optionGood={good} />
-      <StatisticsOfOptionNeutral optionNeutral={neutral} />
-      <StatisticsOfOptionBad optionBad={bad} />
-      <StatisticsAll all={all} />
-      <StatisticsAverage average={average} />
-      <StatisticsPositive positive={positive} />
-      <NoFeedback all={all} />
+      <Button handleClick={handleClickGood} text="good" />
+      <Button handleClick={handleClickNeutral} text="neutral" />
+      <Button handleClick={handleClickBad} text="bad" />
+      <h1>statistis</h1>
+      <StisticLine text="good" statistic={good} />
+      <StisticLine text="neutral" statistic={neutral} />
+      <StisticLine text="bad" statistic={bad} />
+      <StisticLine text="all" statistic={all} />
+      <StisticLine text="average" statistic={average} />
+      <StisticLine text="positive" statistic={positive} />
+      <StisticLine statistic={all == 0 ? "no feedback given" : null} />
     </div>
   )
 }
